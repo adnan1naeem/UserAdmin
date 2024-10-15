cb3287c95853522d86c7db037739c4cd23cb3246
Amir Ali <aali@techtiz.co>
2024-10-15 20:03:35 +0500

    fixing build issue

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,8 @@ import { Box, Typography, CircularProgress } from '@mui/material'; // Import Mat
 import useMediaQuery from '@mui/material/useMediaQuery'; // Import media query hook
 import { motion } from 'framer-motion'; // Import framer-motion for animations
 import UserDrawer from '../components/UserDrawer'; // Import UserDrawer component
-import UserTable from '../components/userTable'; // Import UserTable component
-import Header from '../components/header'; // Import Header component
+import UserTable from '../components/UserTable'; // Import UserTable component
+import Header from '../components/Header'; // Import Header component
 import { getRequest1 } from '../context/api'; // Import API requests
 
 const Home = () => {
